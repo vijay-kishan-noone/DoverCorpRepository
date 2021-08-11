b983cbc931f68ab780d4adf8b1246454d2d625f5
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-11 11:59:27 +0530

    2d Arrays and Jagged Arrays

--- ConsoleApp1/Program17.cs
+++ ConsoleApp1/Program17.cs
@@ -8,7 +8,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            int[,] a = new int[2, 2] { { 1, 2 },{ 3, 4 } }; //Double Dimensional Array
+            //a[0, 0] = 1;
+            //a[0, 1] = 2;
+            //a[1, 0] = 3;
+            //a[1, 1] = 4;
+            Console.WriteLine(a[0,1]);
+            //Jagged Arrays - Array can contain different number of rows and columns
+            int[][] b = new int[2][];
+            b[0] = new int[] { 1, 2 };
+            b[1] = new int[] { 3, 4, 5 };
+            Console.WriteLine("\nJagged Arrays Demonstration\n");
+            for (int i = 0; i < b.Length; i++)
+            {
+                for (int j = 0; j < b[i].Length; j++)
+                {
+                    Console.Write(b[i][j]+" ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
