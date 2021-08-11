50ec23dd99900f4d78cbbd211fffc19cd0299dad
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-11 12:37:25 +0530

    Functions, call by reference example

--- ConsoleApp1/Git.cs
+++ ConsoleApp1/Git.cs
@@ -9,27 +9,33 @@ namespace ConsoleApp1
     }
 }
 /*
- Cloning repository:
+Cloning repository:
 Step1:    Create repository in github website and name it as MyRepo1
+
 Step2:    Create info.txt file in the MyRepo1
-    commit the file
+          commit the file
+
 Step3:    Copy the url of MyRepo1
+
 Step4:    open command prompt in the local computer and clone with the url
-    git clone repourl      
+          git clone repourl      
     
 Step5:    To get all files to local computer
-    git pull
+          git pull
 =====================
 Create a file and push to repostiory
 Step1:    Create a file
-    copy con file4.txt
-    this is my fourthfile
-    ctrl+z to save
+          copy con file4.txt
+          this is my fourthfile
+          ctrl+z to save
+
 Step2:    Add the file to local repository
-    git add file4.txt
+          git add file4.txt
+
 Step3:    Commit the file to staging
-    git commit -m "Message: Filecreated at local computer"
+          git commit -m "Message: Filecreated at local computer"
+
 Step4:    Push to repository
-    git push
+          git push
 
  */
--- ConsoleApp1/Program18.cs
+++ ConsoleApp1/Program18.cs
@@ -6,9 +6,43 @@ namespace ConsoleApp1
 {
     class Program18
     {
+        static void Wishes(string name)
+        {
+            int hour = DateTime.Now.Hour;
+            string wish = string.Empty;
+            if (hour < 12)
+                wish = "Good Morning";
+            else if (hour < 16)
+                wish = "Good Afternoon";
+            else
+                wish = "Good Evening";
+            Console.WriteLine($"Hi {name},{wish}");
+
+        }
+        static void Display(int a, int b)
+        {
+            Console.WriteLine($"a={a},b={b}");
+        }
+        static void Square(ref int a, ref int b) //call by reference, we use "ref" keyword
+        {
+            a *= a;
+            b *= b;
+        }
         static void Main(string[] args)
         {
-
+            int a = 20, b = 30;
+            Wishes("Vijay");
+            Display(a, b);
+            Square(ref a, ref b);
+            Display(a, b);
         }
     }
 }
+/*
+A method is a function inside a class
+A method is a self contained block of statement to perform a task
+A method can return a value to the caller
+Methdo can be be defined as 
+a) static    :    These methods will be called directly
+b) regular: These methods will be called using object
+*/
