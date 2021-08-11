776120ca5c97ed437b0b5f260246be6ab8b40adc
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-11 13:04:43 +0530

    Importance of "Out"

--- ConsoleApp1/Program19.cs
+++ ConsoleApp1/Program19.cs
@@ -6,9 +6,38 @@ namespace ConsoleApp1
 {
     class Program19
     {
+        static void Initialize(out int a)
+        {
+            a = 0;
+        }
+        static void GetRole(string qualification, out string eligibleRole)
+        {
+            switch (qualification)
+            {
+                case "B.Tech":
+                    eligibleRole = "Programmer";
+                    break;
+                case "MBA":
+                    eligibleRole = "HR";
+                    break;
+                default:
+                    eligibleRole = "Assitant";
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
-
+            int a;  // The reason we are not using ref is ref can't be used for unintialized variables
+            Initialize(out a);
+            Console.WriteLine(a);
+            string qualification = "B.Tech";
+            string eligibleRole;
+            GetRole(qualification, out eligibleRole);
+            Console.WriteLine($"For qualification {qualification}, the eligible role is {eligibleRole}");
         }
     }
+/*
+  out parameter used to have variable without initializtion and changes will be effect to
+   the original commonly used in the case of initialization at definition
+*/
 }
