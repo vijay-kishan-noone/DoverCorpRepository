9f0ae410326d888bffdb0a000520480ca74d909f
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-11 11:35:24 +0530

    Biggest and Smallest in an Integer

--- ConsoleApp1/Program16.cs
+++ ConsoleApp1/Program16.cs
@@ -8,7 +8,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            int[] a = new int[5];
+            int big = 0, small = 0;
+            Console.WriteLine("Please enter any 5 elements");
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+                if (i == 0)
+                {
+                    big = a[i];
+                    small = a[i];
+                }
+                if (a[i] > big)
+                    big = a[i];
+                if (a[i] < small)
+                    small = a[i];
+            }
+            Console.WriteLine($"The smallest element is {small}");
+            Console.WriteLine($"The biggest element is {big}");
         }
     }
 }
