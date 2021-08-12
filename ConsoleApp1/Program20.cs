380dfc1a8f0f83d776688507cffd49e79d16b844
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-12 09:21:06 +0530

    Importance of "params"

--- ConsoleApp1/Program20.cs
+++ ConsoleApp1/Program20.cs
@@ -6,9 +6,25 @@ namespace ConsoleApp1
 {
     class Program20
     {
+        static void DisplayNames(string batchCode,params string[] names)
+        {
+            Console.WriteLine($"Batch Code is: {batchCode}");
+            Console.WriteLine("Give names are:");
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.Write($"{names[i]} ");
+            }
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
-
+            DisplayNames("GET-July","Dover", "Corporation");
+            DisplayNames("Emp-Id","Software","Development", "Engineer");
         }
     }
 }
+/*
+ params type:   Allow to have differnt number of arguments to a function
+Can have only one params type as argument
+can have another variable before to params also
+*/
