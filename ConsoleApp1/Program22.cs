535b1c3101853eb152cbdcb212d4e94a4e9e54c8
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-12 10:04:43 +0530

    Nullable Types

--- ConsoleApp1/Program22.cs
+++ ConsoleApp1/Program22.cs
@@ -8,7 +8,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            int ?a = null; //This is how we declare variables that can be null
+            if(a is null)
+                Console.WriteLine("The given vairable is null");
+            else
+                Console.WriteLine($"The value is {a}");
+            string name = string.Empty;
+            if(string.IsNullOrEmpty(name))
+                Console.WriteLine("The given vairable is null");
+            else
+                Console.WriteLine($"The value is {name}");
         }
     }
 }
+/*
+ * nullbale types: used to have empty value into a variable
+ * by prefix ? to the variable
+ * int ?n=null;
+ * 
+ * To compare
+ * if( n is null)
+ *  statement
+ */
