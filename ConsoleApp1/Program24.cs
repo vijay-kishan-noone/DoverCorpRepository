e12726f166a085b044d6c72953916bbd97fa93e8
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-12 12:05:16 +0530

    OOP's and Access Modifiers

--- ConsoleApp1/Program24.cs
+++ ConsoleApp1/Program24.cs
@@ -4,11 +4,37 @@ using System.Text;
 
 namespace ConsoleApp1
 {
+    class Number
+    {
+        //data members section
+        private int num1;
+
+        //member function section
+        public void SetNumber(int num1)
+        {
+            this.num1 = num1;
+        }
+        public void GetNumber()
+        {
+            Console.WriteLine($"num1 = {this.num1}");
+        }
+
+    }
     class Program24
     {
         static void Main(string[] args)
         {
-
+            Number n1 = new Number();
+            n1.SetNumber(100);
+            n1.GetNumber();
         }
     }
 }
+/*
+Class:    It is a user defined data type
+Object:    Instance of a class
+Date member:    Refers to variable inside a class
+Member function:    Refers to function inside a class
+private:    Access modifier restrict access of member to outside
+public:        Acess modifier allow access of member to outside
+*/
