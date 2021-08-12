4cb77a19197296770cb379799f89a3c23b2319db
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-12 09:44:27 +0530

    Method Overloading simple demonstration

--- ConsoleApp1/Program21.cs
+++ ConsoleApp1/Program21.cs
@@ -6,9 +6,18 @@ namespace ConsoleApp1
 {
     class Program21
     {
+        static void Display()
+        {
+            Console.WriteLine("Hello!");
+        }
+        static void Display(string name) //Method Overloading : Defining multiple methods with same name
+        {
+            Console.WriteLine($"Hello {name}");
+        }
         static void Main(string[] args)
         {
-
+            Display();
+            Display("Vijay");
         }
     }
 }
