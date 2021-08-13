4368a44a6d5de059d88f08a2ee3a2e68ed2258f3
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-13 09:23:22 +0530

    Constructor Demo

--- ConsoleApp1/Program25.cs
+++ ConsoleApp1/Program25.cs
@@ -4,11 +4,26 @@ using System.Text;
 
 namespace ConsoleApp1
 {
+    class ConstructorDemo
+    {
+        public ConstructorDemo()
+        {
+            Console.WriteLine("Object is instantiated");
+        }
+    }
     class Program25
     {
         static void Main(string[] args)
         {
-
+            ConstructorDemo a = new ConstructorDemo();
+            ConstructorDemo b = new ConstructorDemo();
         }
     }
 }
+/*
+constructor:    This method is used to instantiate data members
+This is a special method
+a) Exist with same name of the class
+b) Invoke implcitly
+Scenario:   The initial formalities while joining in company will be done using constructor
+*/
