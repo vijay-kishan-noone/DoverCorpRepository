1173a20ecdbad4def546c429f6d3613ccb363fa9
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-13 09:59:09 +0530

    Constructor Demostration

--- ConsoleApp1/Program25.cs
+++ ConsoleApp1/Program25.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace ConsoleApp1
 {
+    enum ParkingType
+    {
+        TwoWheeler, FourWheeler
+    }
     class ConstructorDemo
     {
         public ConstructorDemo()
@@ -11,12 +15,41 @@ namespace ConsoleApp1
             Console.WriteLine("Object is instantiated");
         }
     }
+    class ParkingSystem
+    {
+        private ParkingType type;
+        private int capacity, amount, price, count;
+
+        public ParkingSystem(ParkingType type, int count)
+        {
+            if(type == ParkingType.TwoWheeler)
+            {
+                capacity = 150;
+                price = 20;
+            }
+            else if(type == ParkingType.FourWheeler)
+            {
+                capacity = 40;
+                price = 40;
+            }
+            this.type = type;
+            this.count = count;
+            this.amount = this.count * this.price;
+        }
+        public void GetDetails()
+        {
+            Console.WriteLine($"The type is {this.type.ToString()}," +
+                $" Capacity is {this.capacity}, Count is {this.count}, Amount is {this.amount}," +
+                $" Price is {this.price}");
+        }
+    }
     class Program25
     {
         static void Main(string[] args)
         {
             ConstructorDemo a = new ConstructorDemo();
-            ConstructorDemo b = new ConstructorDemo();
+            ParkingSystem tw = new ParkingSystem(ParkingType.TwoWheeler, 200);
+            tw.GetDetails();
         }
     }
 }
