586d799026411662a8c816f985785361bb11c9a8
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-13 12:58:52 +0530

    Partial Classes

--- ConsoleApp1/Program32.cs
+++ ConsoleApp1/Program32.cs
@@ -4,11 +4,27 @@ using System.Text;
 
 namespace ConsoleApp1
 {
+    partial class MyClass
+    {
+        public void Display1()
+        {
+            Console.WriteLine("Hello from the Display1");
+        }
+    }
+    partial class MyClass
+    {
+        public void Display2()
+        {
+            Console.WriteLine("Hello from the Display2");
+        }
+    }
     class Program32
     {
         static void Main(string[] args)
         {
-
+            MyClass o = new MyClass();
+            o.Display1();
+            o.Display2();
         }
     }
 }
