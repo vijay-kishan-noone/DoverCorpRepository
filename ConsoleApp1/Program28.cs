d7e7f290393559980ff88f155f3a75126ec6f34e
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-13 11:52:34 +0530

    Multilevel Inheritence, importance of virtual keyword

--- ConsoleApp1/Program28.cs
+++ ConsoleApp1/Program28.cs
@@ -17,19 +17,42 @@ namespace ConsoleApp1
         {
             Console.WriteLine("This is the Intermediate Class");
         }
+        public virtual void Display()
+        {
+            Console.WriteLine("Displaying from Intermediate Class");
+        }
     }
     class ChildClass : IntermediateClass
     {
-        public ChildClass()
+        public ChildClass() : base() //constructor Overriding
         {
             Console.WriteLine("This is the Child Class");
+        }
+        public override void Display()
+        {
+            base.Display(); //Invoking base class method from child class
+            Console.WriteLine("Displaying from Child Class");
         }
     }
     class Program28
     {
         static void Main(string[] args)
         {
-            ChildClass c = new ChildClass();
+            IntermediateClass i;
+            i = new IntermediateClass();
+            i.Display();
+            i = new ChildClass(); // once remove virtual and override to see difference
+            i.Display();
+
         }
     }
 }
+/*
+ * Virtual method :
+ * 
+ * When the object declared for base class and memory allocated to derived , 
+ * the base class method override even memory refers to child class
+ * 
+ * To overcome this, prefix virtual to the base class method and override in child class
+ * Virtual methods can be overloaded
+ */
