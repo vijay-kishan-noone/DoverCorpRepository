585c5fa3c9f67ff21c908e32ea2309f22e7515e2
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-13 11:35:32 +0530

    Inheritance

--- ConsoleApp1/Program27.cs
+++ ConsoleApp1/Program27.cs
@@ -4,11 +4,33 @@ using System.Text;
 
 namespace ConsoleApp1
 {
+    class BaseClass
+    {
+        private int a =1; //accessible within the class
+        protected int b=2; //accessible within and dreived class
+        public int c=3; //accesssible every where
+        public void Display()
+        {
+            Console.WriteLine($"a is {a}, b is {b}, c is {c}");
+        }
+    }
+    class DerivedClass :BaseClass
+    {
+        public void DisplayDerived()
+        {
+            // 'a' is not accessible here
+            Console.WriteLine($"b is {b}, c is {c} ");
+        }
+    }
     class Program27
     {
         static void Main(string[] args)
         {
-                
+            BaseClass b = new BaseClass();
+            DerivedClass d = new DerivedClass();
+            b.Display();
+           // d.Display(); 
+            d.DisplayDerived();
         }
     }
 }
--- ConsoleApp1/Program28.cs
+++ ConsoleApp1/Program28.cs
@@ -4,11 +4,32 @@ using System.Text;
 
 namespace ConsoleApp1
 {
+    class ParentClass
+    {
+        public ParentClass()
+        {
+            Console.WriteLine("This is the Parent Class");
+        }
+    }
+    class IntermediateClass : ParentClass
+    {
+        public IntermediateClass()
+        {
+            Console.WriteLine("This is the Intermediate Class");
+        }
+    }
+    class ChildClass : IntermediateClass
+    {
+        public ChildClass()
+        {
+            Console.WriteLine("This is the Child Class");
+        }
+    }
     class Program28
     {
         static void Main(string[] args)
         {
-
+            ChildClass c = new ChildClass();
         }
     }
 }
