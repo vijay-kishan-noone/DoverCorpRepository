f6320b60e7202dc92663f91044b60eeef653d0b3
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-15 10:35:47 +0530

    Hello World!

--- ConsoleApp1/Program1.cs
+++ ConsoleApp1/Program1.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("Hello World!");
         }
     }
