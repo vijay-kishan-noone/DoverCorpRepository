7cb1d5fb5c6cd67c6f09578a5625dbd50919b014
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-16 12:24:32 +0530

    Action and Func Delegates

--- ConsoleApp1/Program40.cs
+++ ConsoleApp1/Program40.cs
@@ -8,7 +8,40 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            Action display = delegate ()
+            {
+                Console.WriteLine("Hello World!");
+            };
+            Action<string> wishes = delegate (string name)
+            {
+                Console.WriteLine($"Hello {name}");
+            };
+            display();
+            wishes("Vijay");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Func<int, int> Square = delegate (int n)
+            {
+                return n * n;
+            };
+            Func<int, int> Next = delegate (int n)
+             {
+                 return n + 1;
+             };
+            Console.WriteLine(Square(2));
+            Console.WriteLine(Next(1000));
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
         }
     }
 }
+/*
+Delegate:    A delegate is a typesafe funcition pointer
+The object of delegate can be referred to a function or set of functions
+.net provided 3 inbuilt delgates
+Action    :    Refers to a function perform a task and without returning value
+Func    :    Refers to a function  return a value
+Predicate:    Refers to a function return boolean value
+
+Delegate enable us send a function as a parameter to other function
+*/
