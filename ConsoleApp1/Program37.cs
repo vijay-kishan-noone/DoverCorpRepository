8499cebf9d7e349052665b62ae9198f92ad2f9bb
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-16 11:06:51 +0530

    Lists

--- ConsoleApp1/Program37.cs
+++ ConsoleApp1/Program37.cs
@@ -8,7 +8,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            List<string> fruits = new List<string> { "Apple", "Banana", "Orange", "Mangoes", "Grapes" };
+            foreach (string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+            Console.WriteLine();
 
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            List<int> numbers = new List<int>();
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
+/*
+Generic is used to restrict the datatype in collections
+*/
