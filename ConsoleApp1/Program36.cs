f50b02a9c9e5ff01080e68848ab9e006aaa1c63e
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-16 10:46:31 +0530

    Hashtable and SortedList

--- ConsoleApp1/Program36.cs
+++ ConsoleApp1/Program36.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Collections;
 
 namespace ConsoleApp1
 {
@@ -8,6 +7,37 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Hashtable items = new Hashtable(); // Key Value Paired Collection
+            items.Add(1, "Vijay");
+            items.Add(2, "Kishan");
+            items.Add(3, "Dover Employee?");
+            items.Add(4, true);
+
+            Console.WriteLine("Please Enter a key");
+            int key = Convert.ToInt32(Console.ReadLine());
+
+            if(items.Contains(key))
+                Console.WriteLine(items[key]);
+            else
+                Console.WriteLine("Key Doesn't exists...");
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            foreach (DictionaryEntry item in items) //doesn't follow any order
+            {
+                Console.WriteLine($"Key is {item.Key} and Value is {item.Value}");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+
+            SortedList s = new SortedList(items);
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (DictionaryEntry item in s) //It follows order
+            {
+                Console.WriteLine($"Key is {item.Key} and Value is {item.Value}");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
 
         }
     }
