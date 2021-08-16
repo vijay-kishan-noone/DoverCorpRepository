538e3b16016a7bda45407664fec85d504a31c0b2
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-16 21:09:49 +0530

    Caluclating Current Bill

--- ConsoleApp1/Program5.cs
+++ ConsoleApp1/Program5.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp1
             int pmr = Convert.ToInt32(Console.ReadLine());
             int bu = pmr - lmr;
             double ur = 0;
+
             if (bu < 150)
                 ur = 1.75;
             else if (bu < 375)
