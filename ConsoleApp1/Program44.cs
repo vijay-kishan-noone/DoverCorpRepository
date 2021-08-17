d11ce20663f846ab8273da3e680ab77978385e38
Vijay Kishan <vijaykishan333@gmail.com>
2021-08-17 11:43:37 +0530

    SOLID Principles - ISP

--- ConsoleApp1/Program44.cs
+++ ConsoleApp1/Program44.cs
@@ -4,8 +4,61 @@ using System.Text;
 
 namespace ConsoleApp1
 {
+    interface IPrintScanTasks
+    {
+        void PrintContent(string content);
+        void ScanContent(string content);
+        void PhotoCopyContent(string content);
+    }
+    interface IFaxTasks
+    {
+        void FaxContent(string content);
+    }
+    interface IDuplexTaskx
+    {
+        void PrintDuplexContent(string content);
+    }
+    class HpPrinter : IPrintScanTasks, IFaxTasks
+    {
+        public void FaxContent(string content)
+        {
+            Console.WriteLine("Can Send fax to the destination");
+        }
+        public void PhotoCopyContent(string content)
+        {
+            Console.WriteLine("Can take xerox copy");
+        }
+        public void PrintContent(string content)
+        {
+            Console.WriteLine("Can take a printout");
+        }
+        public void ScanContent(string content)
+        {
+            Console.WriteLine("Can scan content to system");
+        }
+    }
+    class CanonPrinter : IPrintScanTasks, IDuplexTaskx
+    {
+        public void PhotoCopyContent(string content)
+        {
+            throw new NotImplementedException();
+        }
+        public void PrintContent(string content)
+        {
+            throw new NotImplementedException();
+        }
+        public void PrintDuplexContent(string content)
+        {
+            throw new NotImplementedException();
+        }
+        public void ScanContent(string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
     class Program44
     {
+
         static void Main(string[] args)
         {
 
